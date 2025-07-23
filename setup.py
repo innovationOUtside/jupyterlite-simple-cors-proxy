bbce494dc58bd605ba64affb31edbb2f3444ee22
Tony Hirst <tony.hirst@gmail.com>
2025-07-23 13:16:17 +0100

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jupyterlite-simple-cors-proxy",
-    version="0.1.14",
+    version="0.1.16",
     packages=find_packages(),
     install_requires=[
         "requests",
